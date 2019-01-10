19d1fc46e8654e934e69f9c6f0b1ed78563b8915
Patrick Spettel <patrick.spettel@aon.at>
2019-01-10 10:32:53 +0100

    Update experimentation and post-processing scripts

--- experimentRotatedKleeMintyProblem.m
+++ experimentRotatedKleeMintyProblem.m
@@ -1,99 +1,102 @@
-%% Example experimental 
-%
-% Benchmarking Random Search (RS) on the Rotated Klee-Minty Problem
-%
-  clear all, clc
-%
+%  experimental setup
 
-% Definition of global variable names for performance logging 
+clear all, clc
+
+% 2,3,5,10,20,40
+DIMENSION=[2,3,5,10,20,40];
+
+
 global target_flag
 global T
 global Targets
 global consumed 
 global best
 
-% Range of problem dimensions [default: 2, 3, 5, 10, 20, 40 , (100)]
-DIMENSION=[2,3,5,10]; %,20,40];
-
 for j=1:length(DIMENSION)
-    
-    %% Problem initialization
-    target_flag         = 0;
-    ListT               = [];
-    input.dim           = DIMENSION(j);
-    
-    % Creation of the Rotated Klee-Minty Problem in dimension 'input.dim'
-    problem             = createRotatedKleeMintyCubeConstraintSystem(input.dim);
-      
-    %% USER specifications
-  
-    % Specify the number of independent runs per constrained problem
-    problem.number_of_runs = 3;  % default #runs = 15
-    
-    % Set budget_multiplier 
-    % Determintes the maximal number of constrained function evaluations
-    % DEFAULT: budget = budget_multiplier * dimension
-    problem.budget_multiplier   = 2*10^4;   % default: 2*10^4
-    problem.budget              = input.dim*problem.budget_multiplier; 
-    
-    % Name of the solver to be benchmarked
-    input.strategy      = 'RandS';
+    target_flag = 0;
         
-    for k=1:problem.number_of_runs
-        target_flag     = 1;
-        best            = [];
-        consumed        = 0;
+    %% USER specified budget of constrained function evaluations
+    problem.budget_multiplier   = 2*10^4;
+        
+    % create the rotated Klee-Minty problem in dimension 'dim'
+    dim                 = DIMENSION(j);
+    budget              = dim*problem.budget_multiplier;
+    problem             = createRotatedKleeMintyCubeConstraintSystem(dim);
+    problem.budget      = budget;
+
+    input.dim = dim;
+
+    %% USER specified INPUT parameter of the individual SOLVER
+    % specify SOLVER
+    input.strategy      = 'RandS';
+    
+    %% start EXPERIMENTAL runs
+    number_of_runs = 15;
+    ListT       = [];
+    for k=1:number_of_runs
+        k
+        target_flag = 1;
+        
+        best        = [];
+        consumed    = 0;
         eval(['[out, global_best]=' input.strategy '(problem,problem.budget,problem.lower_bounds,problem.upper_bounds,input);']);
-        List(k,:)       = [input.dim, global_best.val, global_best.conv, norm(global_best.y-problem.t) consumed];
-        GB{k}           = global_best;
-        ListT{k}        = T;
+        dyn.fev = [consumed];
+        dyn.fit = [global_best.val];
+        dyn.conv = [global_best.conv];
+        List(k,:)=[dim, global_best.val, global_best.conv, norm(global_best.y-problem.t), dyn.fev(end)];
+        
+        GB{k}=global_best.y; 
+        eval(['Dyn' num2str(dim) '{k}=dyn;']);
+        ListT{k}=T;
+        ListTargets{k}=Targets;
+        Dyn{k}=dyn;
         if k==1
-            FEperTarget = Targets(:,1:3);
+            FEperTarget=Targets(:,1:3);
         else
-            FEperTarget = [FEperTarget,Targets(:,3)];
+            FEperTarget=[FEperTarget,Targets(:,3)];
         end
             
     end
-
-    % Gather statistics for algorithm assessment
-    rankingL    = lex_sort(List(:,2),List(:,3));
     
-    besti       = rankingL(1);
-    worsti      = rankingL(problem.number_of_runs);
-    cmedi       = rankingL(floor((problem.number_of_runs+1)/2));
-    med         = List(cmedi,2);
-    cmed        = List(cmedi,3);
+    %[ecdf_data] = assessRotatedKleeMintyPerformance(problem,number_of_runs,FEperTarget,ListTargets);
     
-    if List(cmedi,3) == 0
-        nmed  = List(cmedi,4);
-    else
-        nmed  = NaN;
+    rankingL=lex_sort(List(:,2),List(:,3));
+    
+    besti = rankingL(1);
+    worsti= rankingL(15);
+    cmedi = rankingL(8);
+    med   = List(cmedi,2);
+    cmed  = List(cmedi,3);
+    
+    nmed  = List(cmedi,4);
+    nbest = List(besti,4);
+    
+    median_y = GB{cmedi};
+    ncon  = sum(List(:,3 )== 0);
+    FR = ncon/15;
+    FOPT = problem.fOpt;
+    %Stats(:,j) = [dim  FOPT List(besti,2) med cmed abs(FOPT-med) FR mean(List(:,2)) mean(List(:,3)) mean(List(:,5)) ecdf_data.aRT/dim nbest nmed mean(List(:,4)) std(List(:,4))];
+    Stats(:,j) = [dim  FOPT List(besti,2) med cmed abs(FOPT-med) FR mean(List(:,2)) mean(List(:,3)) mean(List(:,5)) 0 nbest nmed mean(List(:,4)) std(List(:,4))];
+        
+    %eval(['StatsN' num2str(dim) '= [dim  FOPT List(besti,2) med cmed (FOPT-med) FR mean(List(:,2)) mean(List(:,3)) mean(List(:,5)) ecdf_data.aRT/dim nbest nmed mean(List(:,4)) std(List(:,4))]']);
+    eval(['StatsN' num2str(dim) '= [dim  FOPT List(besti,2) med cmed (FOPT-med) FR mean(List(:,2)) mean(List(:,3)) mean(List(:,5)) 0 nbest nmed mean(List(:,4)) std(List(:,4))]']);
+    
+    for k=1:15
+        LL(k)=length(Dyn{k}.fev);
     end
-    if List(besti,3) == 0
-        nbest = List(besti,4);
-    else
-        nbest = NaN;
+    len=min(LL);
+    
+    FES(:,j) = Dyn{1}.fev(1:len);
+    FIT(:,j) = Dyn{1}.fit(1:len);
+    CON(:,j) = Dyn{1}.conv(1:len);
+    for k=2:15
+        FES(:,j) = FES(:,j) + Dyn{k}.fev(1:len)';
+        FIT(:,j) = FIT(:,j) + Dyn{k}.fit(1:len)';
+        CON(:,j) = CON(:,j) + Dyn{k}.conv(1:len)';
     end
-    
-    median_y    = GB{cmedi};
-    ncon        = sum(List(:,3 )== 0);
-    FR          = ncon/problem.number_of_runs;
-    FOPT        = problem.fOpt;
-    
-    if FR==1
-        nmean = mean(List(:,4)); 
-        nstd  = std(List(:,4));
-    elseif FR == 0
-        nmean = NaN; 
-        nstd  = NaN;
-    else
-        idf = find(List(:,3)==0);
-        nmean = mean(List(idf,4)); 
-        nstd  = std(List(idf,4));
-    end
-    
-    % Vector of gathered statistics from all independent runs
-    eval(['StatsN' num2str(input.dim) '= [input.dim  FOPT List(besti,2) med cmed (FOPT-med) FR mean(List(:,4)) mean(List(:,5)) std(List(:,4))];']);
+    FES(:,j) = FES(:,j) ./ 15;
+    FIT(:,j) = FIT(:,j) ./ 15;
+    CON(:,j) = CON(:,j) ./ 15;
 
     problem.number_of_runs = 1000;
     FEperTargetBootstrapped = bootstrap(problem, ...
@@ -101,16 +104,19 @@ for j=1:length(DIMENSION)
                                         problem.number_of_runs, ...
                                         1000, ...
                                         problem.budget);
+    ecdf_data = assessRotatedKleeMintyPerformance(problem, ...
+                                                  FEperTargetBootstrapped);
 
-    % Derive ECDF data from all observed runs
-    ecdf_data{j} = assessRotatedKleeMintyPerformance(problem, ...
-                                                     FEperTargetBootstrapped);
-
-
-    clear FES
-    clear FIT
-    clear CON
+    foldername = [date '_linspaced_rotKM_RS_' input.strategy];
+        efn = exist(foldername);
+        if efn ~= 7
+            mkdir(foldername);
+        end
+    save([foldername '/rotKM_' input.strategy '_Dim' num2str(dim) '.mat'],'problem','input',['Dyn' num2str(dim)],'FES','FIT','CON','ListT','ListTargets','ecdf_data',['StatsN' num2str(dim)] ,'-v7')
+    
+     clear FES
+     clear FIT
+     clear CON
 end
 
-% Create ECDF plots and Tables according to the recommendations
-pprocRotatedKleeMintyProblem()
+ %save(['2018_RotationAddC_KleeMinty_' input.strategy 'Rep.mat'],'problem','input','Stats','-v7')
--- pprocRotatedKleeMintyProblem.m
+++ pprocRotatedKleeMintyProblem.m
@@ -1,107 +1,183 @@
-%%  %%%%%%%%%%%%%%%%%%%%%%%%% postprocessing of algorithm results %%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-%%  %%%%%%%%%%%%%%%%%%%%%%%%% display the ECDF plots of the current run  %%%%%%%%%%%%%%%%%%%%%%%%%
-%
+clear all;
+clc;
 
-foldername = [date '_rotKM_RS_' input.strategy];
-    efn = exist(foldername);
-    if efn ~= 7
-        mkdir(foldername);
-    end
-
-for j=1:length(DIMENSION)
-dim  = DIMENSION(j);
-Eint1{j}  = ecdf_data{j}.IntervalFevals./dim;
-Etar1{j}  = ecdf_data{j}.TargetsPerFevals./(problem.number_of_runs*problem.TarNum);
-eval(['Stats(' num2str(j) ',:) = StatsN' num2str(DIMENSION(j)) ';']);
+foldername = [date '_rotKM_RS_comparison'];
+efn = exist(foldername);
+if efn ~= 7
+  mkdir(foldername);
 end
 
-p1=figure;
-    hndl1=eval(['p' num2str(1)]);
-    axes('Parent',hndl1,'Position',[0.15 0.13 0.75 0.70],'LineWidth',1,'FontSize',14);
-    set(gcf,'color','w');
-    set(gcf,'position',[1 600 800 600]);
-    for j = 1:length(DIMENSION)
-        dim = DIMENSION(j);
-        dis  = 'DisplayName';
-        name = ['N' num2str(dim)];
-        % for each dimension plot the ratio of reached targets against the
-        % needed function evaluations
-        eval(['h' num2str(j) '=plot(log(Eint1{j})/log(10),Etar1{j},dis,name);'])
-        hold on
-    end
-    hleg = legend('show');
-    legend boxoff
-    title(input.strategy)
-    plot([log(problem.budget_multiplier)/log(10) log(problem.budget_multiplier)/log(10)],[0 1],'k','HandleVisibility','off')
-    plot([0 ceil(log(problem.budget_multiplier)/log(10))+0.5],...
-        [(problem.ConTarNum+1)/problem.TarNum...
-        (problem.ConTarNum+1)/problem.TarNum],'--k','HandleVisibility','off')
-    xlim([0 ceil(log(problem.budget_multiplier)/log(10))+0.5])
-    ylim([0 1])
-    xlabel('log(#fevals/dimension)')
-    ylabel('target ratio')
-    set(hleg,'FontSize',14,'Location','bestoutside');
-    grid on
+strategies = cell(0, 1);
 
-     print(gcf,[foldername '/ecdf_' input.strategy '.pdf'],'-dpdf','-r600','-bestfit')
+strategy = struct();
+strategy.name = 'RandS';
+strategy.path = ...
+fullfile('.', ...
+         ['10-Jan-2019_linspaced_rotKM_RS_', ...
+          strategy.name]);
+strategies{end + 1, 1} = strategy;
 
-%%  %%%%%%%%%%%%%%%%%%%%%%%%% median error in the objective space  %%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-p2=figure;
-    hndl2=eval(['p' num2str(2)]);
-    ax2=axes('Parent',hndl2,'Position',[0.15 0.13 0.75 0.70],'LineWidth',1,'FontSize',16);
-    set(gcf,'color','w');
-    set(gcf,'position',[1 600 800 600]);
-    loglog(DIMENSION,abs(Stats(:,6)),'-sb','DisplayName','ES','LineWidth',1,'MarkerSize',8,'LineWidth',1.5)
-    hold on
-    plot([1 50],[10^-8 10^-8],'--k','DisplayName','final target')
-    xlim([1.5 max(DIMENSION)+10])
-%     ylim([10^-9 10^-0])
-    xlabel('dimension')
-    ylabel('median obj. function error')
-    grid on
-    hleg = legend('show');
-    legend boxoff
-    set(hleg,'FontSize',16,'Location','NorthWest');
-        set(ax2,'FontSize',20,'MinorGridlines','none')
-        xticks(DIMENSION)
+nStrategies = size(strategies, 1);
 
-     print(gcf,[foldername '/medianFitError_' input.strategy '.pdf'],'-r600','-dpdf','-bestfit')
+dimensions = [2, 3, 5, 10, 20, 40];
+nDimensions = length(dimensions);
+for k = 1:nStrategies
+  strategies{k, 1}.Eint = cell(nDimensions, 1);
+  strategies{k, 1}.Etar = cell(nDimensions, 1);
+  strategies{k, 1}.Stats = zeros(nDimensions, 15);
+  for j = 1:nDimensions
+    dim = dimensions(j);
+    load(fullfile(strategies{k, 1}.path, ...
+                  ['rotKM_', strategies{k, 1}.name, ...
+                   '_Dim', num2str(dim), '.mat']));
+    eval(['stats = StatsN', num2str(dim), ';']);
+    strategies{k, 1}.Eint{j, 1} = ecdf_data.IntervalFevals ./ dim;
+    strategies{k, 1}.Etar{j, 1} = ecdf_data.TargetsPerFevals ./ ...
+                                  (1000 * problem.TarNum);
+    strategies{k, 1}.Stats(j, :) = stats;
+  end
+end
 
-%%  %%%%%%%%%%%%%%%%%%%%%%%%% mean error in the parameter space  %%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
+problem.budget_multiplier=problem.budget/problem.dim;
+
+for k = 1:nStrategies
+  p1=figure;
+  axes('Parent', p1, ...
+       'Position', [0.15 0.13 0.75 0.70], ...
+       'LineWidth', 1, ...
+       'FontSize', 14);
+  set(gcf,'color','w');
+  set(gcf,'position',[1 600 800 600]);
+  for j = 1:nDimensions
+    dim = dimensions(j);
+    dis  = 'DisplayName';
+    name = ['N' num2str(dim)];
+    plot(log(strategies{k, 1}.Eint{j, 1})/log(10),...
+         strategies{k, 1}.Etar{j, 1},dis,name);
+    hold on;
+  end
+  hleg = legend('show');
+  legend boxoff;
+  title(strategies{k, 1}.name);
+  plot([log(problem.budget/dim)/log(10), ...
+        log(problem.budget/dim)/log(10)],[0 1],'k','HandleVisibility','off')
+  plot([0 ceil(log(problem.budget/dim)/log(10))+0.5],...
+       [(problem.ConTarNum+1)/(problem.ConTarNum+problem.FunTarNum+1)...
+                                (problem.ConTarNum+1)/...
+        (problem.ConTarNum+problem.FunTarNum+1)],'--k',...
+       'HandleVisibility','off');
+  xlim([0 ceil(log(problem.budget/dim)/log(10))+0.5]);
+  ylim([0 1]);
+  xlabel('log(#fevals/dimension)');
+  ylabel('target ratio');
+  set(hleg,'FontSize',14,'Location','bestoutside');
+  grid on;
+  print(gcf, ...
+        fullfile(foldername, ['ecdf_' strategies{k, 1}.name '.pdf']), ...
+        '-dpdf','-r600','-bestfit');
+end
+
 p3=figure;
-    hndl3=eval(['p' num2str(3)]);
-    ax3=axes('Parent',hndl3,'Position',[0.15 0.13 0.75 0.70],'LineWidth',1,'FontSize',16);
-    set(gcf,'color','w');
-    set(gcf,'position',[1 600 800 600]);
-    errorbar(DIMENSION,Stats(1:length(DIMENSION),8),Stats(1:length(DIMENSION),10),':pg','DisplayName', input.strategy,'MarkerSize',8,'LineWidth',1.5)
-    hold on
-    xlim([1.5 max(DIMENSION)+10])
-%     ylim([10^-9 10^-3])
-    xlabel('dimension')
-    ylabel('mean norm error')
-    grid on
-    hleg = legend('show');
-    legend boxoff
-    set(hleg,'FontSize',16,'Location','NorthWest');
-    set(ax3,'FontSize',20,'MinorGridlines','none')
-    xticks(DIMENSION)
-%     yticks([1e-9 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3])
-            
-     print(gcf,[foldername '/meanNormError_' input.strategy '.pdf'],'-r600','-dpdf','-bestfit')
+ax1=axes('Parent',p3, ...
+         'Position',[0.15 0.13 0.75 0.70], ...
+         'LineWidth',1,'FontSize',14);
+set(gcf,'color','w');
+set(gcf,'position',[1 600 800 600]);
+for j = 1:nDimensions
+  subplot(2,3,j);
+  dim = dimensions(j);
+  hold on;
+  for k = 1:nStrategies
+    dis1  = 'DisplayName';
+    name1 = strategies{k, 1}.name;
+    plot(log(strategies{k, 1}.Eint{j, 1})/log(10), ...
+         strategies{k, 1}.Etar{j, 1},dis1,name1);
+  end
+  plot([log(problem.budget_multiplier)/log(10), ...
+        log(problem.budget_multiplier)/log(10)],[0 1], ...
+       'k','HandleVisibility','off');
+  title(['N=' num2str(dim)]);
+  plot([0 ceil(log(problem.budget_multiplier)/log(10))+0.5],...
+       [(problem.ConTarNum+1)/(problem.ConTarNum+problem.FunTarNum+1)...
+                                (problem.ConTarNum+1)/...
+        (problem.ConTarNum+problem.FunTarNum+1)],'--k',...
+       'HandleVisibility','off');
+  xlim([0 ceil(log(problem.budget_multiplier)/log(10))+0.5]);
+  ylim([0 1]);
+  xlabel('log(#fevals/dimension)');
+  ylabel('target ratio');
+  grid on;
+  hleg = legend('show');
+  legend boxoff;
+  set(hleg,'FontSize',8,'Location','southoutside');
+end
+print(gcf, ...
+      fullfile(foldername, 'ecdf_comparison.pdf'), ...
+      '-dpdf','-r600','-bestfit');
 
-%%   %%%%%%%%%%%%%%%%%%%%%%%%% print Statistics to TXT-File %%%%%%%%%%%%%%%%%%%%%%%%%
-%
+p4=figure;
+ax2=axes('Parent',p4,...
+         'Position',[0.15 0.13 0.75 0.70],...
+         'LineWidth',1,'FontSize',16);
+set(gcf,'color','w');
+set(gcf,'position',[1 600 800 600]);
+hold on;
+for k = 1:nStrategies
+  loglog(dimensions,abs(strategies{k, 1}.Stats(:,6)), ...
+         'DisplayName',strategies{k, 1}.name,...
+         'LineWidth',1,'MarkerSize',8,'LineWidth',1.5);
+end
+plot([1 50],[10^-8 10^-8],'--k','DisplayName','final target');
+xlim([1.5 max(dimensions)+10]);
+%ylim([10^-9 10^-0]);
+xlabel('dimension');
+ylabel('median obj. function error');
+grid on;
+hleg = legend('show');
+legend boxoff;
+set(hleg,'FontSize',16,'Location','NorthWest');
+set(ax2,'FontSize',20,'MinorGridlines','none');
+xticks(dimensions);
+print(gcf,fullfile(foldername, 'medianFitError_comparison.pdf'), ...
+      '-r600','-dpdf','-bestfit');
 
-    filename = strcat([input.strategy '_RS_on_RotKM_Table.txt']);
-    fileID = fopen([foldername '/' filename],'w');        
-    fprintf(fileID,'%14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\n','Dim','Fopt','Fbest','Fmed','\nu','medianErr','FR','meanNormErr','Fevals');
-    for j=1:length(DIMENSION)
-        fprintf(fileID,'%14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\n',Stats(j,1:9));
-    end
-    fclose(fileID);
+p3=figure;
+ax3=axes('Parent',p3,...
+         'Position',[0.15 0.13 0.75 0.70],'LineWidth',1,'FontSize',16);
+set(gcf,'color','w');
+set(gcf,'position',[1 600 800 600]);
+hold on;
+for k = 1:nStrategies
+  errorbar(dimensions,strategies{k, 1}.Stats(1:length(dimensions),14),...
+           strategies{k, 1}.Stats(1:length(dimensions),15),...
+           'DisplayName', strategies{k, 1}.name,'MarkerSize',8,'LineWidth',1.5);
+end
+xlim([1.5 max(dimensions)+10]);
+%ylim([10^-9 10^-3]);
+xlabel('dimension');
+ylabel('mean norm error');
+grid on;
+hleg = legend('show');
+legend boxoff;
+set(hleg,'FontSize',16,'Location','NorthWest');
+set(ax3,'FontSize',20,'MinorGridlines','none');
+xticks(dimensions);
+%yticks([1e-9 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3]);
+print(gcf,fullfile(foldername, 'meanNormError_comparison.pdf'), ...
+      '-r600','-dpdf','-bestfit');
+
+for k = 1:nStrategies
+  filename = strcat(strategies{k, 1}.name, '_RS_on_RotKM_Table.txt');
+  fileID = fopen(fullfile(foldername, filename),'w');
+  fprintf(fileID, ...
+          '%14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\t %14s\n', ...
+          'Dim','Fopt','Fbest','Fmed','\nu', ...
+          'medianErr','FR','meanNormErr','Fevals');
+  for j=1:length(dimensions)
+    fprintf(fileID,['%14.8g\t %14.8g\t %14.8g\t %14.8g\t ', ...
+                    '%14.8g\t %14.8g\t %14.8g\t %14.8g\t %14.8g\n'],...
+            strategies{k, 1}.Stats(j,[1:7, 14, 10]));
+  end
+  fclose(fileID);
+end
+
